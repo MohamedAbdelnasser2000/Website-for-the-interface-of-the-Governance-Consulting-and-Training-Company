4a66bb3eb1e5a00c30592646b13ce0467bb77c0c
MohamedAbdelnasser2000 <99351983+MohamedAbdelnasser2000@users.noreply.github.com>
2024-12-29 19:07:11 +0200

    Add files via upload

---
+++ Controllers/ContactServicesController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GovEgy_2.Models;
+
+namespace GovEgy_2.Controllers
+{
+    public class ContactServicesController : Controller
+    {
+        private GovEgyDB2 db = new GovEgyDB2();
+
+        // GET: ContactServices
+        public ActionResult Index()
+        {
+            var contactServices = db.ContactServices.Include(c => c.ServiceType_2);
+            return View(contactServices.ToList());
+        }
+
+        // GET: ContactServices/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContactService contactService = db.ContactServices.Find(id);
+            if (contactService == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactService);
+        }
+
+        // GET: ContactServices/Create
+        public ActionResult Create()
+        {
+            ViewBag.S_id = new SelectList(db.ServiceType_2, "S_id", "ServiceType");
+            return View();
+        }
+
+        // POST: ContactServices/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Phone,Email,Service_Type,Details,S_id")] ContactService contactService)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ContactServices.Add(contactService);
+                db.SaveChanges();
+                return RedirectToAction("Message");
+            }
+
+            ViewBag.S_id = new SelectList(db.ServiceType_2, "S_id", "ServiceType", contactService.S_id);
+            return View(contactService);
+        }
+
+        // GET: ContactServices/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContactService contactService = db.ContactServices.Find(id);
+            if (contactService == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.S_id = new SelectList(db.ServiceType_2, "S_id", "ServiceType", contactService.S_id);
+            return View(contactService);
+        }
+
+        // POST: ContactServices/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Phone,Email,Service_Type,Details,S_id")] ContactService contactService)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contactService).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.S_id = new SelectList(db.ServiceType_2, "S_id", "ServiceType", contactService.S_id);
+            return View(contactService);
+        }
+
+        // GET: ContactServices/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContactService contactService = db.ContactServices.Find(id);
+            if (contactService == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactService);
+        }
+
+        // POST: ContactServices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ContactService contactService = db.ContactServices.Find(id);
+            db.ContactServices.Remove(contactService);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        public ActionResult Message()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/CoursController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GovEgy_2.Models;
+
+namespace GovEgy_2.Controllers
+{
+    public class CoursController : Controller
+    {
+        private GovEgyDB2 db = new GovEgyDB2();
+
+        // GET: Cours
+        public ActionResult Index(string department)
+        {
+            var courses = db.Courses.Include(c => c.Department);
+            if (!String.IsNullOrEmpty(department))
+            {
+                courses = courses.Where(p => p.Department.D_Name == department);
+            }
+            return View(courses.ToList());
+        }
+
+
+
+
+        // GET: Cours/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cours cours = db.Courses.Find(id);
+            if (cours == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cours);
+        }
+
+        // GET: Cours/Create
+        public ActionResult Create()
+        {
+            ViewBag.D_Id = new SelectList(db.Departments, "D_Id", "D_Name");
+            return View();
+        }
+
+        // POST: Cours/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,C_Name,Price,Discount,C_Number,C_Mode,Discription,C_Image,C_File,D_Id")] Cours cours,HttpPostedFileBase file)
+        {
+            if (ModelState.IsValid)
+            {
+                //..\\Images\\
+                if (file!=null)
+                {
+                    file.SaveAs(HttpContext.Server.MapPath("~/Content/Image/For_Reser/") + file.FileName);
+                    cours.C_Image = file.FileName;
+                }
+
+
+
+
+                db.Courses.Add(cours);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.D_Id = new SelectList(db.Departments, "D_Id", "D_Name", cours.D_Id);
+            return View(cours);
+        }
+
+        // GET: Cours/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cours cours = db.Courses.Find(id);
+            if (cours == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.D_Id = new SelectList(db.Departments, "D_Id", "D_Name", cours.D_Id);
+            return View(cours);
+        }
+
+        // POST: Cours/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,C_Name,Price,Discount,C_Number,C_Mode,Discription,C_Image,C_File,D_Id")] Cours cours)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cours).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.D_Id = new SelectList(db.Departments, "D_Id", "D_Name", cours.D_Id);
+            return View(cours);
+        }
+
+        // GET: Cours/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cours cours = db.Courses.Find(id);
+            if (cours == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cours);
+        }
+
+        // POST: Cours/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cours cours = db.Courses.Find(id);
+            db.Courses.Remove(cours);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
